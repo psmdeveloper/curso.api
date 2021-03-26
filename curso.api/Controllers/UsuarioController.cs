8367dec88e960aa0528be2d738fb5fd8e6980629
Philipp da Silva Moreira <psmdeveloper@gmail.com>
2021-03-26 11:25:55 -0300

    Configuracoes para personalizacao de documentacao gerada pelo package swagger.

--- curso.api/Controllers/UsuarioController.cs
+++ curso.api/Controllers/UsuarioController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using curso.api.Models.Usuarios;
+using curso.api.Models;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace curso.api.Controllers
 {
@@ -7,13 +9,34 @@ namespace curso.api.Controllers
 	[ApiController]
 	public class UsuarioController : ControllerBase
 	{
+		/// <summary>
+		/// Proporciona a acao de logar.
+		/// </summary>
+		/// <param name="Login">
+		/// O formato do objeto esperado é: {Login, Senha}
+		/// </param>
+		/// <returns>
+		/// O formato do objeto esperado é: {Login, Senha}
+		/// </returns>
 		[HttpPost]
 		[Route("Login")]
+		[SwaggerResponse(statusCode: 200, description: "Sucesso ao autenticar", type: typeof(LoginViewModelInput))]
+		[SwaggerResponse(statusCode: 400, description: "Campos Obrigatórios.", type: typeof(ValidaCampoViewModelOutput))]
+		[SwaggerResponse(statusCode: 500, description: "Erro interno.", type: typeof(ErroGenericoViewModel))]
 		public IActionResult Logar(LoginViewModelInput loginViewModelInput)
 		{
 			return Ok(loginViewModelInput);
 		}
 
+		/// <summary>
+		/// Proporciona a acao de registrar um usuario.
+		/// </summary>
+		/// <param name="Registrar">
+		/// O formato do objeto esperado é: {Login, E-mail, Senha}
+		/// </param>
+		/// <returns>
+		/// O formato do objeto esperado é: {Login, E-mail, Senha}
+		/// </returns>
 		[HttpPost]
 		[Route("Registrar")]
 		public IActionResult Registrar(RegistroViewModelInput registroViewModelInput)
--- curso.api/Startup.cs
+++ curso.api/Startup.cs
@@ -1,15 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
 
 namespace curso.api
 {
@@ -25,6 +21,27 @@ namespace curso.api
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddSwaggerGen(d =>
+				{
+					/* 
+					 * Montando o nome do arquivo de origem com a informacao de documentacao; Que neste caso, 
+					 * é o mesmo arquivo, gerado em tempo de build, após a configuracao das propriedades do projeto (gerar arquivo XML)
+					*/
+					var xmlArquivo = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+
+					/*
+					 * Obtendo o caminho fisico do assembly deste projeto, para compor a localizacao completa do arquivo contendo as descricoes
+					 * desejadas de documentacao do projeto/API.
+					 */
+					var xmlCaminho = Path.Combine(AppContext.BaseDirectory, xmlArquivo);
+
+					/* 
+					 * Informando"/configurando ao " servico do swagger a origem da documentacao a ser exposta/externalizada
+					 * para consumidores da API.
+					 */
+					d.IncludeXmlComments(xmlCaminho);
+				});
+
 			services.AddControllers();
 		}
 
@@ -45,6 +62,19 @@ namespace curso.api
 			app.UseEndpoints(endpoints =>
 			{
 				endpoints.MapControllers();
+			});
+
+			// Adicionando o middleware do swagger ao Pipeline de requisicoes da API
+			app.UseSwagger();
+
+			/*
+			 * Sobrescrevendo o comportamento de rotas, adicionando, que para se obtrer acesso
+			 * ao Swagger, nenhum prefixo de rota sera necessário
+			 */
+			app.UseSwaggerUI(r =>
+			{
+				r.SwaggerEndpoint("/swagger/v1/swagger.json", "Api curso DIO");
+				r.RoutePrefix = string.Empty; // swagger
 			});
 		}
 	}
