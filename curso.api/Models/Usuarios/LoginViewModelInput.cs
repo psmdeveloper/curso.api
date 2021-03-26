605cb634fd33df294dd2cc93c97a74c311a150a1
Philipp da Silva Moreira <psmdeveloper@gmail.com>
2021-03-26 11:19:04 -0300

    Revert "- Personalizacao de configuracao do swagger, para fins de documentacao detalhada da API."
    This reverts commit 70f6909d9a8195a91e1e0d206e94907ffbb5d2b8.

--- curso.api/Models/Usuarios/LoginViewModelInput.cs
+++ curso.api/Models/Usuarios/LoginViewModelInput.cs
@@ -1,13 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
-
+﻿
 namespace curso.api.Models.Usuarios
 {
 	public class LoginViewModelInput
 	{
 		#region Propriedades
-		[Required(ErrorMessage ="O Login é obrigatório.")]
 		public string Login { get; set; }
-		[Required(ErrorMessage ="A Senha é obrigatória.")]
 		public string Senha { get; set; }
 		#endregion
 
