eadc7fef032801f9105a4dc96bf31fbc99c34587
Philipp da Silva Moreira <psmdeveloper@gmail.com>
2021-03-26 09:12:58 -0300

    Criado personalizacao das rotas de chamada das actions/acoes.

--- curso.api/Controllers/UsuarioController.cs
+++ curso.api/Controllers/UsuarioController.cs
@@ -3,17 +3,19 @@ using curso.api.Models.Usuarios;
 
 namespace curso.api.Controllers
 {
-	[Route("api/[controller]")]
+	[Route("api/v1/[controller]")]
 	[ApiController]
 	public class UsuarioController : ControllerBase
 	{
 		[HttpPost]
+		[Route("Login")]
 		public IActionResult Logar(LoginViewModelInput loginViewModelInput)
 		{
 			return Ok(loginViewModelInput);
 		}
 
 		[HttpPost]
+		[Route("Registrar")]
 		public IActionResult Registrar(RegistroViewModelInput registroViewModelInput)
 		{
 			return Created(nameof(UsuarioController), registroViewModelInput);
